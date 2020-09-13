381fa5fecdae12f1bc9acbeb1637b013a09cd361
WarrenMarshall <warren@warrenmarshall.biz>
2020-09-13 19:25:17 -0400

    Checkpoint

--- src/endless_breakout/app_main.cpp
+++ src/endless_breakout/app_main.cpp
@@ -46,7 +46,7 @@ void endless_breakout_game::add_score( int points )
 
 	if( ( score % 100 ) == 0 )
 	{
-		brick_move_speed -= 5;
+		brick_move_speed -= 3;
 		brick_move_speed = w_max( brick_move_speed, 50 );
 
 		timer_brick_move->interval_ms = static_cast<float>( brick_move_speed );
