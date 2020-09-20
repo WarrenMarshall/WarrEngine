b1435f85e02d6957aacbcf77564a31bcbbf14da8
WarrenMarshall <warren@warrenmarshall.biz>
2020-09-20 10:12:21 -0400

    "hello_world" project cleaned up and sterilized

--- src/hello_world/layer_default.h
+++ src/hello_world/layer_default.h
@@ -1,34 +1,11 @@
 #pragma once
 
-struct w_contact_listener : b2ContactListener
-{
-	std::vector<w_vec2> points;
-	w_vec2 normal;
-
-	// Called when two fixtures begin to touch
-	virtual void BeginContact( b2Contact* contact ) override;
-
-	// Called when two fixtures cease to touch
-	virtual void EndContact( b2Contact* contact ) override;
-
-	virtual void PreSolve( b2Contact* contact, const b2Manifold* oldManifold ) override;
-	virtual void PostSolve( b2Contact* contact, const b2ContactImpulse* impulse ) override;
-};
-
 struct layer_default : w_layer
 {
-	w_contact_listener contact_listener;
-	w_vec2 player_trace_hit;
-	bool player_on_ground = true;
+	a_gradient* gradient = nullptr;
 
 	layer_default();
 
-	w_entity* player = nullptr;
-	w_entity* player2 = nullptr;
-	w_entity* world_geo = nullptr;
-
 	virtual void push() override;
-	virtual void update() override;
 	virtual void draw() override;
-	virtual bool handle_input_event( const w_input_event* evt );
 };
