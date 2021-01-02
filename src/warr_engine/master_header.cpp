8a208c6dbbbe884c1299c070083fa042bac1fe50
WarrenMarshall <warren@warrenmarshall.biz>
2021-01-02 09:51:33 -0500

    adding export line to automatically use AMD GPUs as well as NVIDIA on laptops

--- src/warr_engine/master_header.cpp
+++ src/warr_engine/master_header.cpp
@@ -12,14 +12,19 @@ std::unique_ptr<w_engine> engine = nullptr;
 std::unique_ptr<w_game> base_game = nullptr;
 
 // yoinked from : http://stevendebock.blogspot.com/2013/07/nvidia-optimus.html
+// also : https://www.reddit.com/r/gamedev/comments/bk7xbe/psa_for_anyone_developing_a_gameengine_in_c/
 //
 // this line fixes the problem where laptops would select their integrated
-// graphics cards instead of the nvidia one. i don't love this but it's
-// simple and it works, so it's fine for now.
+// graphics cards instead of the nvidia/AMD one.
 //
 // it's certainly better than making people go into their nvidia
 // control panel and changing their GPU selection setting.
 
-extern "C" {
-	_declspec( dllexport ) DWORD NvOptimusEnablement = 0x00000001;
+extern "C"
+{
+	// nvidia
+	__declspec( dllexport ) DWORD NvOptimusEnablement = 0x00000001;
+
+	// AMD
+	__declspec( dllexport ) int AmdPowerXpressRequestHighPerformance = 1;
 }
