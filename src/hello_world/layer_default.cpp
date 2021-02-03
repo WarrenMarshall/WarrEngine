a3756a8f946ba6b7ee39e51211ba21302b73353b
Warren Marshall <warren@warrenmarshall.biz>
2021-02-03 06:26:02 -0500

    cleaning up "hello world" project

--- src/hello_world/layer_default.cpp
+++ src/hello_world/layer_default.cpp
@@ -11,7 +11,8 @@ void layer_default::push()
 	gradient = a_texture::find( "background_gradient" );
 	tex_hello_world = a_texture::find( "tex_hello_world" );
 	movement_tween = w_tween( -175, 175, 2000, tween_type::pingpong, tween_via::sinusoidal );
-	scale_tween = w_tween( 1.0f, 5.0f, 4500, tween_type::pingpong, tween_via::quadratic );
+	scale_tween = w_tween( 1.0f, 5.0f, 4750, tween_type::pingpong, tween_via::quadratic );
+	tilt_tween = w_tween( -25.0f, 25.0f, 10000, tween_type::pingpong, tween_via::cubic );
 
 	engine->window->set_mouse_mode( mouse_mode::os );
 }
@@ -23,5 +24,6 @@ void layer_default::draw()
 	RENDER
 		->draw( gradient, w_rect( 0, 0, v_window_w, v_window_h ) )
 		->push_scale( *scale_tween )
+		->push_angle( *tilt_tween )
 		->draw_sprite( tex_hello_world, { v_window_hw + *movement_tween, v_window_hh } );
 }
--- src/hello_world/layer_default.h
+++ src/hello_world/layer_default.h
@@ -6,7 +6,7 @@ struct layer_default : w_layer
 {
 	a_texture* gradient = nullptr;
 	a_texture* tex_hello_world = nullptr;
-	w_tween movement_tween, scale_tween;
+	w_tween movement_tween, scale_tween, tilt_tween;
 
 	layer_default();
 
