32dca60fd8f063fa00c8e75a4266a36e876476f1
Warren Marshall <warren@warrenmarshall.biz>
2021-06-06 11:15:34 -0400

    cleaning up some old files

--- src/template_platformer/player_input_receiver.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-struct w_player_input_receiver : i_input_receiver
-{
-	virtual bool iir_on_motion( const w_input_event* evt ) override;
-	virtual bool iir_on_pressed( const w_input_event* evt ) override;
-};
